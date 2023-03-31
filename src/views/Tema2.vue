6ccb1854c4e6f491760fe0870ed9b7493b03973f
jorozcop <orojacobo@gmail.com>
2023-03-31 18:48:08 -0500

    videos + material descargable

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -25,7 +25,7 @@
 
     figure.mt-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/HWz5XFAtFyI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
       figcaption Video: Leyenda del video  
 
     .row.mt-3
